429e179f828bd7375253753adb45c9fe3fab1f24
ihsan taofik <ihsantaofik1700@gmail.com>
2020-08-13 17:28:16 +0700

    hapus spasi

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
 		<p><a href="#">FansPage</a> || <a href="#"> Instagram </a> || <a href="#"> Twitter </a> 
-		<br> Copyrght © 2020 Mobil Indonesia.com</p>
+		<br> Copyrght © 2020 MobilIndonesia.com</p>
 </div>
