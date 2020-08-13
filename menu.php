e82fcdf66c6156991cc431ace4773903c8f71df6
ihsan taofik <ihsantaofik1700@gmail.com>
2020-08-13 17:27:50 +0700

    geser menu utama

--- menu.php
+++ menu.php
@@ -8,9 +8,9 @@
 			<li><a href="index.php">HOME</a></li>
 			<li><a href="kendaraan.php">JENIS KENDARAAN</a></li>
 			<li><a href="testimoni.php">TESTIMONI</a></li>
-			<li><a href="sosmed.php">HUBUNGI KAMI</a></li>
-			<li><a href="about.php">ABOUT</a></li>
+			<li><a href="sosmed.php">HUBUNGI KAMI</a></li>			
 			<li><a href="penyewaan.php">PENYEWAAN</a></li>
+			<li><a href="about.php">ABOUT</a></li>
 		</ul>
 	</div>
 	<br class="clear">
